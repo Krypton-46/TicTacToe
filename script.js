7a182fb6f7f0453108d2f370af6e833e2a9a2eed
HT LAPTOP <HT LAPTOP@DESKTOP-J9GATOO>
2025-06-20 20:04:44 +0500

    almost done

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-let img = document.querySelector('img');
-img.getAttribute('class');
-img.setAttribute('class', 'new-class');
+let button = document.querySelector('button');
+button.addEventListener('click', () => {
+    let p = document.querySelector('p');
+    p.remove();
+    button.remove();
+    let div=document.querySelector('.container');
+    div.classList.remove('hidden');
+});
